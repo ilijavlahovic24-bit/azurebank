9e8452c853996be9ea9517ddeb6ef4904ff7cf54
ilijavlahovic24-bit <ilija.vlahovic24@gmail.com>
2026-08-16 16:47:32 +0200

    account model and store

--- internal/accounts/model.go
+++ internal/accounts/model.go
@@ -1,1 +1,11 @@
 package accounts
+
+import "time"
+
+type Account struct {
+	ID        int64     `json:"id"`
+	UserID    int64     `json:"user_id"`
+	Type      string    `json:"type"`    // "checking" | "savings"
+	Balance   int64     `json:"balance"` // u dinarima/centima, nikad float
+	CreatedAt time.Time `json:"created_at"`
+}
--- internal/accounts/store.go
+++ internal/accounts/store.go
@@ -1,1 +1,9 @@
 package accounts
+
+import "context"
+
+type Store interface {
+	CreateAccount(ctx context.Context, userID int64, accountType string) (*Account, error)
+	GetAccountsByUserID(ctx context.Context, userID int64) ([]Account, error)
+	GetAccountByID(ctx context.Context, id, userID int64) (*Account, error)
+}
